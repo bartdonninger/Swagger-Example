b1e6e1df5bd2fe8125d72823ad68fa4ec855443e
Bart Donninger <bart.donninger@pggmenco.nl>
2018-04-10 13:23:37 +0200

    Add API key and try to add custom headers to SwaggerUI

--- Swagger-Example/Startup.cs
+++ Swagger-Example/Startup.cs
@@ -12,7 +12,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using NJsonSchema;
+using NSwag;
 using NSwag.AspNetCore;
+using NSwag.SwaggerGeneration.Processors;
+using NSwag.SwaggerGeneration.Processors.Contexts;
+using NSwag.SwaggerGeneration.Processors.Security;
 
 namespace Swagger_Example
 {
@@ -34,7 +38,8 @@ namespace Swagger_Example
                     builder => builder.WithOrigins("http://localhost:60109").AllowAnyMethod().AllowAnyHeader());
             });
             services.AddMvc();
-            
+            services.AddSwagger();
+
 
         }
 
@@ -51,12 +56,41 @@ namespace Swagger_Example
             }
 
             // Enable the Swagger UI middleware and the Swagger generator
-            app.UseSwaggerUi(typeof(Startup).GetTypeInfo().Assembly, settings =>
+            app.UseSwaggerUi3(typeof(Startup).GetTypeInfo().Assembly, settings =>
             {
                 settings.GeneratorSettings.DefaultPropertyNameHandling = PropertyNameHandling.CamelCase;
+                settings.GeneratorSettings.Title = "Test Swagger API";
+                settings.GeneratorSettings.Title = "Test Swagger Description";
+                settings.GeneratorSettings.DocumentProcessors.Add(new SecurityDefinitionAppender("apikey",
+                    new SwaggerSecurityScheme
+                    {
+                        Type = SwaggerSecuritySchemeType.ApiKey,
+                        Name = "api_key",
+                        In = SwaggerSecurityApiKeyLocation.Header,
+                        
+                    }));
+                settings.GeneratorSettings.DocumentProcessors.Add(new MyDocumentProcessor());
             });
 
             app.UseMvc();
+        }
+
+        public class MyDocumentProcessor : IDocumentProcessor
+        {
+
+            // TODO: This is not included in the header :(
+            public async Task ProcessAsync(DocumentProcessorContext context)
+            {
+                context.Document.Parameters.Add("test", new SwaggerParameter
+                    {
+                        Name = "TestHeader",
+                        Type = JsonObjectType.String,
+                        Kind = SwaggerParameterKind.Header,
+                        IsRequired = true,
+                        Description = "blaa description"
+                    }
+                );
+            }
         }
     }
 }
